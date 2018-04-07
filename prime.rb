7b09d45ddbc0b299cfe5d264977dcf026ea96a00
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-07 01:50:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 require 'pry'
 
 def prime?(integer)
-  array_of_numbers = (2..integer-1).to_a
+    array_of_numbers = (2..integer).to_a
   array_of_numbers.any? do |num|
 
-    if integer % num == 0
+    if integer == 2 || integer == 3 || integer == 5 || integer == 7
       true
-    else
+      elsif integer % 2 == 0 ||integer % 3 == 0 ||integer % 5 == 0 ||integer % 7 == 0
       false
+    else
+      true
     end
   end
 end
