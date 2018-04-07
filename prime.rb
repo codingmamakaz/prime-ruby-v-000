78333010d3bd8ef8cedac4de6e2ea5be2a7b1ec5
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-07 01:05:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+  array_of_numbers = (2..integer-1).to_a
+  array_of_numbers.any? do |num|
+    
+    if integer % num == 0  
+      false
+    else
+      true
+    end
+  end  
+end    
+ 
